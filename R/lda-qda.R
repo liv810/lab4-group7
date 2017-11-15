0d4c1b81cd2bcbc1d610c16c34a9be834d214e31
yizhouzhao <yizhou_zhao@berkeley.edu>
2017-11-15 13:43:45 -0800

    Update lda-qda.R

--- R/lda-qda.R
+++ R/lda-qda.R
@@ -6,7 +6,7 @@ library(ROCR)
 library(ggplot2)
 
 qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
-  # Trains and evalutes QDA and LDA models with three-fold cross-validation
+  # Trains and evalutes QDA and LDA models with three-fold cross-validation, to get the ROC curves for plotting
   #
   # Arguments:
   #   images: traning data with label images
@@ -40,6 +40,7 @@ qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
                  data.frame(TPR = qda.roc2@y.values[[1]], FPR = qda.roc2@x.values[[1]], fold = factor(2)),
                  data.frame(TPR = qda.roc3@y.values[[1]], FPR = qda.roc3@x.values[[1]], fold = factor(3)))
   
+  #plotting ROC curves  and AUC scores for QDA model
   qda.roc.info = ggplot(qda.cv) +
     geom_line(aes(x=FPR,y=TPR, group=fold, color=fold)) +
     scale_color_discrete(name = "Predictions on Image Num",
@@ -48,23 +49,27 @@ qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
                                     "3 (AUC=0.895/0.887)")) +
     ggtitle("QDA")
   
-  #lda part
+  #LDA with one-image-left-out CV
   lda1 = lda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 1))
   lda2 = lda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 2))
   lda3 = lda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 3))
   
+  #predict the left-out image with QDA
   lda1.pred = predict(lda1, type="prob",newdata=Qimage1)
   lda2.pred = predict(lda2, type="prob",newdata=Qimage2)
   lda3.pred = predict(lda3, type="prob",newdata=Qimage3)
   
+  #ROC values and ROC curves
   lda.roc1 = performance(prediction(lda1.pred$posterior[,2], Qimage1$label), "tpr", "fpr")
   lda.roc2 = performance(prediction(lda2.pred$posterior[,2], Qimage2$label), "tpr", "fpr")
   lda.roc3 = performance(prediction(lda3.pred$posterior[,2], Qimage3$label), "tpr", "fpr")
   
+  #AUC scores
   lda.auc1 = performance(prediction(lda1.pred$posterior[,2], Qimage1$label), "auc")@y.values[[1]]
   lda.auc2 = performance(prediction(lda2.pred$posterior[,2], Qimage2$label), "auc")@y.values[[1]]
   lda.auc3 = performance(prediction(lda3.pred$posterior[,2], Qimage3$label), "auc")@y.values[[1]]
   
+  #plotting ROC curves  and AUC scores for LDA model
   lda.cv = rbind(data.frame(TPR = lda.roc1@y.values[[1]], FPR = lda.roc1@x.values[[1]], fold = factor(1)),
                  data.frame(TPR = lda.roc2@y.values[[1]], FPR = lda.roc2@x.values[[1]], fold = factor(2)),
                  data.frame(TPR = lda.roc3@y.values[[1]], FPR = lda.roc3@x.values[[1]], fold = factor(3)))
@@ -77,6 +82,7 @@ qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
                                     "3 (AUC=0.895/0.887)")) +
     ggtitle("LDA")
     
+  #return the two plots
   return(list(qda.roc.info,lda.roc.info))
 }
 
@@ -123,7 +129,8 @@ qda.confustion <- function(Qimage1,Qimage2,Qimage3,method = "qda"){
   
   #confusion plot information
   ggconf <- list(geom_point(aes(x=x, y=y, group=confusion, colour=confusion), alpha=0.1),
-                 scale_colour_manual(values = c("TP" = "gray70","FP" = "turquoise2","TN" = "lightgoldenrod1","FN" = "deeppink", "Unknown" = "#777777")), theme(legend.position="right", aspect.ratio=1), guides(color=guide_legend(override.aes=list(size = 2.5, alpha=0.5))))
+                 scale_colour_manual(values = c("TP" = "gray70","FP" = "turquoise2","TN" = "lightgoldenrod1","FN" = "deeppink", "Unknown" = "#777777")), 
+                 theme(legend.position="right", aspect.ratio=1), guides(color=guide_legend(override.aes=list(size = 2.5, alpha=0.5))))
   
   # theme to remove axis
   no_axis <- theme(axis.line=element_blank(),
@@ -141,5 +148,6 @@ qda.confustion <- function(Qimage1,Qimage2,Qimage3,method = "qda"){
   qda3.conf <- ggplot(Qimage3) + ggconf +
     ggtitle("Image 3") + blank_theme + no_axis  
 
+  #return list of two plots
   return(list(qda1.conf,qda2.conf,qda3.conf))
 }
