0b23ce9d69648254bccfd9e57ba74d874517b4ee
yizhouzhao <yizhou_zhao@berkeley.edu>
2017-11-15 14:10:59 -0800

    Update lda-qda.R

--- R/lda-qda.R
+++ R/lda-qda.R
@@ -5,7 +5,7 @@ library(MASS)
 library(ROCR)
 library(ggplot2)
 
-qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
+qda.lda.info <- function(labeled_image1,labeled_image2,labeled_image3){
   # Trains and evalutes QDA and LDA models with three-fold cross-validation, to get the ROC curves for plotting
   #
   # Arguments:
@@ -13,7 +13,7 @@ qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
   # Returns: plotting information with lda and qda out put
   # Attachments: auc scores from LDA and QDA
   
-  images = rbind(Qimage1, Qimage2, Qimage3)
+  images = rbind(labeled_image1, labeled_image2, labeled_image3)
   
   #QDA with one-image-left-out CV
   qda1 = qda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 1))
@@ -21,19 +21,19 @@ qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
   qda3 = qda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 3))
   
   #predict the left-out image with QDA
-  qda1.pred = predict(qda1, type="prob",newdata=Qimage1)
-  qda2.pred = predict(qda2, type="prob",newdata=Qimage2)
-  qda3.pred = predict(qda3, type="prob",newdata=Qimage3)
+  qda1.pred = predict(qda1, type="prob",newdata=labeled_image1)
+  qda2.pred = predict(qda2, type="prob",newdata=labeled_image2)
+  qda3.pred = predict(qda3, type="prob",newdata=labeled_image3)
   
   #ROC values and ROC curves
-  qda.roc1 = performance(prediction(qda1.pred$posterior[,2], Qimage1$label), "tpr", "fpr")
-  qda.roc2 = performance(prediction(qda2.pred$posterior[,2], Qimage2$label), "tpr", "fpr")
-  qda.roc3 = performance(prediction(qda3.pred$posterior[,2], Qimage3$label), "tpr", "fpr")
+  qda.roc1 = performance(prediction(qda1.pred$posterior[,2], labeled_image1$label), "tpr", "fpr")
+  qda.roc2 = performance(prediction(qda2.pred$posterior[,2], labeled_image2$label), "tpr", "fpr")
+  qda.roc3 = performance(prediction(qda3.pred$posterior[,2], labeled_image3$label), "tpr", "fpr")
   
   #AUC scores
-  qda.auc1 = performance(prediction(qda1.pred$posterior[,2], Qimage1$label), "auc")@y.values[[1]]
-  qda.auc2 = performance(prediction(qda2.pred$posterior[,2], Qimage2$label), "auc")@y.values[[1]]
-  qda.auc3 = performance(prediction(qda3.pred$posterior[,2], Qimage3$label), "auc")@y.values[[1]]
+  qda.auc1 = performance(prediction(qda1.pred$posterior[,2], labeled_image1$label), "auc")@y.values[[1]]
+  qda.auc2 = performance(prediction(qda2.pred$posterior[,2], labeled_image2$label), "auc")@y.values[[1]]
+  qda.auc3 = performance(prediction(qda3.pred$posterior[,2], labeled_image3$label), "auc")@y.values[[1]]
   
   #Plot the ROC curve
   qda.cv = rbind(data.frame(TPR = qda.roc1@y.values[[1]], FPR = qda.roc1@x.values[[1]], fold = factor(1)),
@@ -55,19 +55,19 @@ qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
   lda3 = lda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 3))
   
   #predict the left-out image with QDA
-  lda1.pred = predict(lda1, type="prob",newdata=Qimage1)
-  lda2.pred = predict(lda2, type="prob",newdata=Qimage2)
-  lda3.pred = predict(lda3, type="prob",newdata=Qimage3)
+  lda1.pred = predict(lda1, type="prob",newdata=labeled_image1)
+  lda2.pred = predict(lda2, type="prob",newdata=labeled_image2)
+  lda3.pred = predict(lda3, type="prob",newdata=labeled_image3)
   
   #ROC values and ROC curves
-  lda.roc1 = performance(prediction(lda1.pred$posterior[,2], Qimage1$label), "tpr", "fpr")
-  lda.roc2 = performance(prediction(lda2.pred$posterior[,2], Qimage2$label), "tpr", "fpr")
-  lda.roc3 = performance(prediction(lda3.pred$posterior[,2], Qimage3$label), "tpr", "fpr")
+  lda.roc1 = performance(prediction(lda1.pred$posterior[,2], labeled_image1$label), "tpr", "fpr")
+  lda.roc2 = performance(prediction(lda2.pred$posterior[,2], labeled_image2$label), "tpr", "fpr")
+  lda.roc3 = performance(prediction(lda3.pred$posterior[,2], labeled_image3$label), "tpr", "fpr")
   
   #AUC scores
-  lda.auc1 = performance(prediction(lda1.pred$posterior[,2], Qimage1$label), "auc")@y.values[[1]]
-  lda.auc2 = performance(prediction(lda2.pred$posterior[,2], Qimage2$label), "auc")@y.values[[1]]
-  lda.auc3 = performance(prediction(lda3.pred$posterior[,2], Qimage3$label), "auc")@y.values[[1]]
+  lda.auc1 = performance(prediction(lda1.pred$posterior[,2], labeled_image1$label), "auc")@y.values[[1]]
+  lda.auc2 = performance(prediction(lda2.pred$posterior[,2], labeled_image2$label), "auc")@y.values[[1]]
+  lda.auc3 = performance(prediction(lda3.pred$posterior[,2], labeled_image3$label), "auc")@y.values[[1]]
   
   #plotting ROC curves  and AUC scores for LDA model
   lda.cv = rbind(data.frame(TPR = lda.roc1@y.values[[1]], FPR = lda.roc1@x.values[[1]], fold = factor(1)),
@@ -86,13 +86,13 @@ qda.lda.info <- function(Qimage1,Qimage2,Qimage3){
   return(list(qda.roc.info,lda.roc.info))
 }
 
-qda.confustion <- function(Qimage1,Qimage2,Qimage3,method = "qda"){
+qda.confustion <- function(labeled_image1,labeled_image2,labeled_image3,method = "qda"){
   # Get the confusion plot for QDA classification
   # Arguments:
   #   images: traning data with label images
   # Returns: confusion plots in the three-fold cv.
   
-  images = rbind(Qimage1, Qimage2, Qimage3)
+  images = rbind(labeled_image1, labeled_image2, labeled_image3)
   
   #QDA with one-image-left-out CV
   qda1 = qda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 1))
@@ -100,14 +100,14 @@ qda.confustion <- function(Qimage1,Qimage2,Qimage3,method = "qda"){
   qda3 = qda(label ~ NDAI + SD + CORR, data = images %>% filter(Image != 3))
   
   #predict the left-out image with QDA
-  qda1.pred = predict(qda1, type="prob",newdata=Qimage1)
-  qda2.pred = predict(qda2, type="prob",newdata=Qimage2)
-  qda3.pred = predict(qda3, type="prob",newdata=Qimage3)
+  qda1.pred = predict(qda1, type="prob",newdata=labeled_image1)
+  qda2.pred = predict(qda2, type="prob",newdata=labeled_image2)
+  qda3.pred = predict(qda3, type="prob",newdata=labeled_image3)
   
   #ROC values and ROC curves
-  qda.roc1 = performance(prediction(qda1.pred$posterior[,2], Qimage1$label), "tpr", "fpr")
-  qda.roc2 = performance(prediction(qda2.pred$posterior[,2], Qimage2$label), "tpr", "fpr")
-  qda.roc3 = performance(prediction(qda3.pred$posterior[,2], Qimage3$label), "tpr", "fpr")
+  qda.roc1 = performance(prediction(qda1.pred$posterior[,2], labeled_image1$label), "tpr", "fpr")
+  qda.roc2 = performance(prediction(qda2.pred$posterior[,2], labeled_image2$label), "tpr", "fpr")
+  qda.roc3 = performance(prediction(qda3.pred$posterior[,2], labeled_image3$label), "tpr", "fpr")
   
   #Find the best thresholds in the three cross-validations (alpha-values)
   qda.thresh1 = qda.roc1@alpha.values[[1]][which.max(qda.roc1@y.values[[1]] - qda.roc1@x.values[[1]])]
@@ -118,13 +118,13 @@ qda.confustion <- function(Qimage1,Qimage2,Qimage3,method = "qda"){
   thresh = mean(c(qda.thresh1,qda.thresh2,qda.thresh3)) #0.21
   
   #Get the confusion plots
-  Qimage1 = Qimage1 %>% mutate(posterior = qda1.pred$posterior[,2]) %>% 
+  labeled_image1 = labeled_image1 %>% mutate(posterior = qda1.pred$posterior[,2]) %>% 
     mutate(confusion = ifelse(label == -1,ifelse(posterior < thresh,"TN","FN"),ifelse(posterior >= thresh,"TP","FP")))
   
-  Qimage2 = Qimage2 %>% mutate(posterior = qda2.pred$posterior[,2]) %>% 
+  labeled_image2 = labeled_image2 %>% mutate(posterior = qda2.pred$posterior[,2]) %>% 
     mutate(confusion = ifelse(label == -1,ifelse(posterior < thresh,"TN","FN"),ifelse(posterior >= thresh,"TP","FP")))
   
-  Qimage3 = Qimage3 %>% mutate(posterior = qda3.pred$posterior[,2]) %>% 
+  labeled_image3 = labeled_image3 %>% mutate(posterior = qda3.pred$posterior[,2]) %>% 
     mutate(confusion = ifelse(label == -1,ifelse(posterior < thresh,"TN","FN"),ifelse(posterior >= thresh,"TP","FP")))
   
   #confusion plot information
@@ -141,11 +141,11 @@ qda.confustion <- function(Qimage1,Qimage2,Qimage3,method = "qda"){
                    axis.title.x=element_blank())
   
   #get the confusion plots
-  qda1.conf <- ggplot(Qimage1) + ggconf +
+  qda1.conf <- ggplot(labeled_image1) + ggconf +
     ggtitle("Image 1") + blank_theme + no_axis   
-  qda2.conf <- ggplot(Qimage2) + ggconf +
+  qda2.conf <- ggplot(labeled_image2) + ggconf +
     ggtitle("Image 2") + blank_theme + no_axis
-  qda3.conf <- ggplot(Qimage3) + ggconf +
+  qda3.conf <- ggplot(labeled_image3) + ggconf +
     ggtitle("Image 3") + blank_theme + no_axis  
 
   #return list of two plots
